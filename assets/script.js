979a5f15de8f38d66aecaa4d4de0ee5c56d74446
Kevin Lewis <kevin.lewis@vonage.com>
2020-03-18 12:10:43 +0000

    frontend reg

---
+++ assets/script.js
@@ -0,0 +1,62 @@
+const app = new Vue({
+  el: '#form',
+  data: {
+    submitted: false,
+    success: false,
+    error: false,
+    countries: [ 'Afghanistan','Åland Islands','Albania','Algeria','American Samoa','AndorrA','Angola','Anguilla','Antarctica','Antigua and Barbuda','Argentina','Armenia','Aruba','Australia','Austria','Azerbaijan','Bahamas','Bahrain','Bangladesh','Barbados','Belarus','Belgium','Belize','Benin','Bermuda','Bhutan','Bolivia','Bosnia and Herzegovina','Botswana','Bouvet Island','Brazil','British Indian Ocean Territory','Brunei Darussalam','Bulgaria','Burkina Faso','Burundi','Cambodia','Cameroon','Canada','Cape Verde','Cayman Islands','Central African Republic','Chad','Chile','China','Christmas Island','Cocos (Keeling) Islands','Colombia','Comoros','Congo','Congo, The Democratic Republic of the','Cook Islands','Costa Rica','Cote D\'Ivoire','Croatia','Cuba','Cyprus','Czech Republic','Denmark','Djibouti','Dominica','Dominican Republic','Ecuador','Egypt','El Salvador','Equatorial Guinea','Eritrea','Estonia','Ethiopia','Falkland Islands (Malvinas)','Faroe Islands','Fiji','Finland','France','French Guiana','French Polynesia','French Southern Territories','Gabon','Gambia','Georgia','Germany','Ghana','Gibraltar','Greece','Greenland','Grenada','Guadeloupe','Guam','Guatemala','Guernsey','Guinea','Guinea-Bissau','Guyana','Haiti','Heard Island and Mcdonald Islands','Holy See (Vatican City State)','Honduras','Hong Kong','Hungary','Iceland','India','Indonesia','Iran, Islamic Republic Of','Iraq','Ireland','Isle of Man','Israel','Italy','Jamaica','Japan','Jersey','Jordan','Kazakhstan','Kenya','Kiribati','Korea, Democratic People\'S Republic of','Korea, Republic of','Kuwait','Kyrgyzstan','Lao People\'S Democratic Republic','Latvia','Lebanon','Lesotho','Liberia','Libyan Arab Jamahiriya','Liechtenstein','Lithuania','Luxembourg','Macao','Macedonia, The Former Yugoslav Republic of','Madagascar','Malawi','Malaysia','Maldives','Mali','Malta','Marshall Islands','Martinique','Mauritania','Mauritius','Mayotte','Mexico','Micronesia, Federated States of','Moldova, Republic of','Monaco','Mongolia','Montserrat','Morocco','Mozambique','Myanmar','Namibia','Nauru','Nepal','Netherlands','Netherlands Antilles','New Caledonia','New Zealand','Nicaragua','Niger','Nigeria','Niue','Norfolk Island','Northern Mariana Islands','Norway','Oman','Pakistan','Palau','Palestinian Territory, Occupied','Panama','Papua New Guinea','Paraguay','Peru','Philippines','Pitcairn','Poland','Portugal','Puerto Rico','Qatar','Reunion','Romania','Russian Federation','RWANDA','Saint Helena','Saint Kitts and Nevis','Saint Lucia','Saint Pierre and Miquelon','Saint Vincent and the Grenadines','Samoa','San Marino','Sao Tome and Principe','Saudi Arabia','Senegal','Serbia and Montenegro','Seychelles','Sierra Leone','Singapore','Slovakia','Slovenia','Solomon Islands','Somalia','South Africa','South Georgia and the South Sandwich Islands','Spain','Sri Lanka','Sudan','Suriname','Svalbard and Jan Mayen','Swaziland','Sweden','Switzerland','Syrian Arab Republic','Taiwan, Province of China','Tajikistan','Tanzania, United Republic of','Thailand','Timor-Leste','Togo','Tokelau','Tonga','Trinidad and Tobago','Tunisia','Turkey','Turkmenistan','Turks and Caicos Islands','Tuvalu','Uganda','Ukraine','United Arab Emirates','United Kingdom','United States','United States Minor Outlying Islands','Uruguay','Uzbekistan','Vanuatu','Venezuela','Viet Nam','Virgin Islands, British','Virgin Islands, U.S.','Wallis and Futuna','Western Sahara','Yemen','Zambia','Zimbabwe' ],
+    form: {
+      newsletter: false,
+      address: {}
+    },
+    response: undefined
+  },
+  methods: {
+    submit() {
+      this.error = false;
+      this.submitted = true;
+      
+      const { first_name, last_name, email, newsletter, address: { address1, locality, postal_code, country } } = this.form;
+
+      if(!first_name || !last_name || !email) {
+        this.error = 'You must fill in all required fields - at minimum your first name, last name and email address.'
+        this.submitted = false;
+        return;
+      }
+
+      const partialAddress = address1 || locality || postal_code || country;
+      const fullAddress = address1 && locality && postal_code && country;
+
+      if(partialAddress && !fullAddress) {
+        this.error = 'If you want to receive stickers, you must fill in all address fields'
+        this.submitted = false;
+        return;
+      }
+
+      let payload = { first_name, last_name, email, newsletter };
+      if(fullAddress) {
+        payload = { ...payload, address1, locality, postal_code, country }
+      }
+
+      axios({
+        method: 'POST',
+        url: 'http://localhost:9000/register',
+        data: payload
+      }).then(({ data }) => {
+        if(data.error) {
+          this.submitted = false;
+          this.error = data.error;
+          return;
+        } else {
+          this.response = data;
+          this.success = true;
+          return;
+        }
+      }).catch(err => {
+        console.log(err);
+        this.error = 'There was an error registering. If this problem persists please email us.'
+        this.submitted = false;
+      }) 
+    }
+  }
+})
---
+++ functions/src/register.js
@@ -0,0 +1,165 @@
+require('dotenv').config()
+
+const axios = require('axios')
+
+const base = require('airtable').base(process.env.AIRTABLE_BASE_ID);
+
+const SmartyStreetsSDK = require("smartystreets-javascript-sdk");
+const SmartyStreetsCore = SmartyStreetsSDK.core;
+const Lookup = SmartyStreetsSDK.internationalStreet.Lookup;
+const credentials = new SmartyStreetsCore.StaticCredentials(process.env.SMARTY_AUTH_ID, process.env.SMARTY_AUTH_TOKEN);
+let smarty = SmartyStreetsCore.buildClient.internationalStreet(credentials);
+
+const SparkPost = require('sparkpost');
+const sparkpost = new SparkPost(process.env.SPARKPOST_API_KEY);
+
+const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type'
+}
+
+exports.handler = async (event, context) => {
+    try {
+        if (event.httpMethod !== 'POST') {
+            return { headers, statusCode: 200, body: JSON.stringify({ error: 'Invalid method' }) }
+        }
+
+        const { first_name, last_name, email, address1, locality, postal_code, country, newsletter } = JSON.parse(event.body);
+        const hasProvidedAddress = address1 && locality && postal_code && country;
+        
+        if(!first_name || !last_name || !email) {
+            return { headers, statusCode: 500, body: JSON.stringify({ error: 'You must provide your first name, last name and email.' }) }
+        }
+
+        if(await checkIfEmailExists(email)) {
+            return { headers, statusCode: 500, body: JSON.stringify({ error: 'You have already registered.' }) }
+        } else {
+            let fullAddress = false;
+            if(hasProvidedAddress) fullAddress = await validateAddress(address1, locality, postal_code, country);
+
+            await createAttendee(first_name, last_name, email, fullAddress.address, fullAddress.address_verified);
+            await sendEmail(email, fullAddress);
+            if(newsletter) await subscribeToNewsletter(email)
+
+            if(fullAddress) {
+                return { headers, statusCode: 200, body: JSON.stringify({address: true, verified: fullAddress.address_verified}) }
+            } else {
+                return { headers, statusCode: 200, body: JSON.stringify({address: false}) }
+            }
+        }
+    } catch (err) {
+        console.error(err)
+        return { headers, statusCode: 500, body: JSON.stringify({ error: 'We had trouble registering you. If this error persists please email us.' }) }
+    }
+}
+
+async function checkIfEmailExists(email) {
+    return new Promise((resolve, reject) => {
+        base('Attendees').select({
+            filterByFormula: `({email}="${email}")`
+        }).eachPage(records => {
+            resolve(records.length > 0)
+        }, err => {
+            if (err) { reject(new Error(err)) }
+        });
+    })
+}
+
+async function validateAddress(address1, locality, postal_code, country) {
+    return new Promise((resolve, reject) => {
+        let address = new Lookup();
+        address.country = country;
+        address.address1 = address1;
+        address.postal_code = postal_code;
+        address.locality = locality;
+
+        smarty.send(address).then(results => {
+            const result = results.result[0];
+            const { analysis: { verificationStatus: verify, addressPrecision: precision } } = result;
+            const isVerified = verify == "Verified" && (precision == "Premise" || precision == "DeliveryPoint")
+            
+            let completedAddress = `${address1}, ${locality}, ${postal_code}, ${country}`;
+            if(isVerified) {
+                let a = [];
+                for(let i=1; i<13; i++) {
+                    if(result[`address${i}`]) a.push(result[`address${i}`])
+                }
+                completedAddress = a.join(', ')
+            }
+            
+            resolve({ address: completedAddress, address_verified: isVerified })
+        }).catch(err => {
+            console.error(err)
+            reject(new Error(err))
+        })
+    })
+}
+
+async function createAttendee(first_name, last_name, email, address, address_verified) {
+    return new Promise((resolve, reject) => {
+        base('Attendees').create({ 
+            first_name, last_name , email, address, address_verified
+        }, err => {
+            if (err) { return reject(new Error(err)) }
+            return resolve(true)
+        });
+    });
+}
+
+async function sendEmail(email, address) {
+    return new Promise((resolve, reject) => {
+        let message = `
+            <p>Hey there,</p>
+            <p>Thank you so much for registering to take part in You Got This 2020: From Home. We hope you will enjoy the event.</p>
+            <p>Please feel free to share the event with your friends and colleagues so we can positively impact as many people as possible.<p>
+        `
+        if(address && address.address_verified) {
+            message += `<p>We'll be sending out stickers closer to the event. We successfully verified your address so there shouldn't be any issues with getting them to you.</p>`
+        } 
+        if(address && !address.address_verified) {
+            message += `<p>We'll be sending out stickers closer to the event. Just to let you know - we struggled to verify your address. If ${address.address} is correct, there shouldn't be a problem, but if it's incorrect please get in touch with us so we can amend it.</p>`
+        }
+        message += `
+            <p>We'll send you a couple of important updates before the day, and if you want more regular updates consider <a href="https://twitter.com/yougotthisconf">following us on Twitter.</a></p>
+            <p>Much love</p>
+            <p><a href="https://twitter.com/_phzn">Kevin</a> & <a href="https://twitter.com/ShyRuparel">Shy</a> - the You Got This 2020: From Home team</p>
+        `
+        sparkpost.transmissions.send({
+            content: {
+                from: {
+                    name: 'You Got This Team',
+                    email: 'kevin@yougotthis.io'
+                },
+                subject: 'Registration for You Got This 2020: From Home',
+                html: message
+            },
+            recipients: [{ address: email }]
+        }).then(data => {
+            resolve(true)
+        }).catch(err => {
+            console.error(err);
+            reject(new Error(err))
+        })
+    });
+}
+
+async function subscribeToNewsletter(email) {
+    return new Promise((resolve, reject) => {
+        axios({
+            url: 'https://api.buttondown.email/v1/subscribers',
+            method: 'POST',
+            headers: {
+                Authorization: `Token ${process.env.BUTTONDOWN_API_KEY}`
+            },
+            data: {
+                email,
+                tags: ['home-2020']
+            }
+        }).then(data => {
+            resolve(true)
+        }).catch(err => {
+            console.error(err);
+            resolve(true)
+        })
+    })
+}
